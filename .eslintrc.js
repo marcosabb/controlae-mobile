08b09f14b3c3e6a02c7ab5355f5f968145a38608
Marcos Borges <marcos.abb52@gmail.com>
2020-02-06 20:56:22 -0300

    feat: add lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
