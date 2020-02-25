b08a07d116050acc9a6f9e759c385a2a54547b7e
Marcos Borges <marcos.abb52@gmail.com>
2020-02-25 15:27:00 -0300

    feat(app): add slash screen

--- android/app/src/main/java/com/controlae/MainActivity.java
+++ android/app/src/main/java/com/controlae/MainActivity.java
@@ -1,11 +1,20 @@
 package com.controlae;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this, R.style.SplashScreenTheme);
+      super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import 'react-native-gesture-handler'
 import '~/config/reactotron'
 
-import React from 'react'
+import React, { useEffect } from 'react'
+import SplashScreen from 'react-native-splash-screen'
 
 import Routes from './routes'
 
 import { ThemeProvider } from '~/contexts/theme'
 
 export default function App () {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <ThemeProvider>
       <Routes />
