e3fec713bf0b128e2b57c0b9531baa478f2715f1
Marcos Borges <marcos.abb52@gmail.com>
2020-02-27 09:38:40 -0300

    fix(logs): clean logs

--- src/services/infrared.js
+++ src/services/infrared.js
@@ -2,7 +2,6 @@ import { hasIrEmitter, transmitProntoCode } from 'react-native-ir-manager'
 
 export async function transmit (code) {
   try {
-    console.log(code)
     if (code && await hasIrEmitter()) await transmitProntoCode(code)
   } catch (error) {
     console.log(error)
